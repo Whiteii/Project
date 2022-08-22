00c8aa9ab8aea98afa1d7e33875ba3888df31e28
Liang <lianghui732@gmail.com>
2022-08-22 09:54:14 -0400

    Population Standard Deviation Vs Sample Standard Deviation

---
+++ population_STD_vs_sample_STD.py
@@ -0,0 +1,53 @@
+from pickle import APPEND
+from socketserver import DatagramRequestHandler
+import std 
+import random 
+import math 
+import matplotlib as plt 
+
+#Name: Liang Zhang 
+#Email Address: Lianghui732@gmail.com
+# generate 1000 random data. This represent as a population variance 
+
+population_random_data = []
+for i in range(20000):
+    random_data = random.randint(1,1000)
+    population_random_data.append(random_data) 
+
+print('population_random_data:',population_random_data)
+
+data = population_random_data 
+population_variance = std.calculate_population_variance(data,accumualted_variance= 0)
+print('true population_variance:',population_variance)
+population_standard_deviation = math.sqrt(population_variance)
+print('true population_standard_deviation:',population_standard_deviation)  
+# Treat This The True Value Of Standard Deviation And Variance
+###################################################################################################################################################################################################################################################################################################################  
+
+
+#randomly select sample of data from population_random_data 
+
+for i in range(10,5050,10):
+    random_sample_data = std.sample_of_data(population_random_data,i)
+    #print('random_sample_data:',random_sample_data)
+    # using population variance formula to calculate 
+ 
+    population_variance_from_sample_data = std.calculate_population_variance(random_sample_data,accumualted_variance = 0) 
+    print('population_variance_from_sample_data:', population_variance_from_sample_data,'# of sample:',len(random_sample_data)) 
+       
+    # using sample_variance_from_sample_data formula to calculate 
+    sample_variance_from_sample_data = std.calculate_sample_variance(random_sample_data, accumualted_variance= 0)   
+    print('sample_variance_from_sample_data:',sample_variance_from_sample_data,'# of sample:',len(random_sample_data))
+
+
+
+
+
+
+
+
+ 
+
+
+
+    
---
+++ std.py
@@ -0,0 +1,46 @@
+from random import sample
+from re import A
+from statistics import variance
+import numpy
+import math
+import random
+
+
+# convert a string into a list
+def Convert(string):
+    li = list(string.split(","))
+    return li
+ 
+# calculate population variance
+def calculate_population_variance(str1, accumualted_variance = 0):  
+    res = str1
+    population_mean = sum(res)/len(res)
+    for i in range(0,len(res)):
+        population_variance = math.pow(res[i] - population_mean,2)
+        accumualted_variance = (population_variance + accumualted_variance)
+        variance = accumualted_variance/len(res)
+        #print(variance)
+        #print(len(str1))
+          
+    return variance
+
+# calculate sample variance
+def calculate_sample_variance(str1,accumualted_variance = 0):  
+    res = str1 
+    sample_mean = sum(res)/len(res)
+    for i in range(0,len(res)):
+        sample_variance = math.pow(res[i] - sample_mean,2)
+        accumualted_variance = (sample_variance + accumualted_variance)
+        variance = accumualted_variance/(len(res) - 1)
+    return variance    
+
+#randomly selected a data from a list and store into a new list
+def sample_of_data(list,amount_of_data):    
+    sample_collection = []
+    for i in range(0,amount_of_data):
+        random_data = random.randint(1,1000)
+        sample_data = list[random_data]
+        sample_collection.append(sample_data)
+    return sample_collection
+
+

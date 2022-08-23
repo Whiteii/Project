17a29580a536684065c0d9d50e325bb96c6c99e3
Liang <lianghui732@gmail.com>
2022-08-23 14:29:30 -0400

    robust_central_tendency function

--- population_vs_sample.py
+++ population_vs_sample.py
@@ -23,12 +23,12 @@ for i in range(80000):
     random_data = np.random.randint(1,20)
     population_random_data.append(random_data) 
 
-#print('population_random_data:',population_random_data)
+print('population_random_data:',population_random_data)
 data = population_random_data 
 population_variance = std.calculate_population_variance(data,accumualted_variance= 0)
-#print('true population_variance:',population_variance)
+print('true population_variance:',population_variance)
 population_standard_deviation = math.sqrt(population_variance)
-#print('true population_standard_deviation:',population_standard_deviation)  
+print('true population_standard_deviation:',population_standard_deviation)  
 # Treat This The True Value Of Standard Deviation And Variance
 ###################################################################################################################################################################################################################################################################################################################  
 
@@ -51,31 +51,27 @@ for i in range(1,200,1):
     
     #population_variance_from_sample_data
     population_variance_from_sample_data = std.calculate_population_variance(list,accumualted_variance = 0)  
-    #print('population_variance_from_sample_data:',population_variance_from_sample_data)  
-   # plt.annotate(b + 'real variance',[0,population_variance],fontsize = 10, c = 'black')
-    #plt.axhline(y = population_variance, c='darkblue',linewidth=2,alpha=0.5) 
-    #plt.pause(0.10)
-   # plt.plot(len(list),population_variance_from_sample_data, color = 'red', alpha = .75, marker = 'o', markersize = 5)  
-
-
+    print('population_variance_from_sample_data:',population_variance_from_sample_data)  
+    plt.annotate(b + 'real variance',[0,population_variance],fontsize = 10, c = 'black')
+    plt.axhline(y = population_variance, c='darkblue',linewidth=2,alpha=0.5) 
+    plt.pause(0.10)
+    plt.plot(len(list),population_variance_from_sample_data, color = 'red', alpha = .75, marker = 'o', markersize = 5)  
 
     # sample_variance_from_sample_data
-    #plt.figure(1)  
-    #plt.axhline(y = population_variance, c='darkblue',linewidth=2,alpha=0.5) 
+    plt.figure(1)  
+    plt.axhline(y = population_variance, c='darkblue',linewidth=2,alpha=0.5) 
     sample_variance_from_sample_data = std.calculate_sample_variance(list,accumualted_variance=0)
-    #print('sample_variance_from_sample_data:',sample_variance_from_sample_data)
-    #plt.plot(len(list),sample_variance_from_sample_data,color = 'black', alpha = .75,marker = 'o', markersize = 5)
+    print('sample_variance_from_sample_data:',sample_variance_from_sample_data)
+    plt.plot(len(list),sample_variance_from_sample_data,color = 'black', alpha = .75,marker = 'o', markersize = 5)
  
     # look at these data and see how far are they away from the true variance.  
       
       
-#plt.show()  
+plt.show()  
 
 
 
-list = [5,9,5,5,7,11]
-b = std.mean_absolute_deviation(list,mad=0)
-print(b)
+
 
 
  
--- std.py
+++ std.py
@@ -1,8 +1,8 @@
 from cgitb import reset
 from lib2to3.pytree import convert
 from random import sample
-from re import A
-from statistics import variance
+from re import A, I
+from statistics import median, variance
 import numpy as np
 import math
 import random
@@ -11,7 +11,47 @@ import random
 
 # this is a module I'm currently working on and updating. 
 
+#sorting the list in ascending order
+def sorting_list(list): 
+    for i in range(0,len(list)):
+        for j in range(i + 1,len(list)):
+            if list[j] < list[i] : # so one scenario is when i = 1 and j = 0. That where b < c. 
+                  temp = list[i] #5
+                  list[i] = list[j] #4
+                  list[j] = temp # 5
+    return list 
+        
 
+
+#IQR 
+num_list = [35,50,50,50,56,60,60,75,1]
+# check if the even or odd.
+
+# If the arithmetic_mean > median => we use central-tendency and IQR 
+def robust_central_tendency(list):
+    new_list = sorting_list(list)
+    print(new_list)
+    arithmetic_mean = sum(new_list)/len(new_list)
+    print('arithmetic_mean:',arithmetic_mean)
+    if len(new_list)%2 == 0:
+        i = int(len(new_list)/2)
+        median =(new_list[i-1] + list[i])/2
+        print('median:',median)
+    elif len(list)%2 == 1:
+        i = int((len(new_list)-1)/2)
+        median = (new_list[i])
+        print('median:',median)
+        if arithmetic_mean > median:
+            print('use median as central-tendency and IQR')
+        else: 
+            print('use arthmetic mean as central-tendency and standard deviation')
+
+  
+robust_central_tendency(num_list)
+  
+  
+  
+            
 # convert a string into a list
 def Convert(string):
     li = list(string.split(','))

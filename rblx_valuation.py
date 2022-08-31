ebd81b12de4aee855e7cb95aa54ffcb1082897a4
Liang <lianghui732@gmail.com>
2022-08-30 22:23:51 -0400

    update

--- rblx_valuation.py
+++ rblx_valuation.py
@@ -9,13 +9,12 @@ from scipy.stats import norm
 #Market Risk Return : 0.09 
 #Beta : dafault is 1 
 #Ticker: RBLX Monthly Return is [0.07,0.14,0.23,-0.04,-0.16,0.6,-0.8,0.11,0.25]
-
 cost_of_equity = finance.CAPM(0.02,0.011,[0.07,0.14,0.23,-0.04,-0.16,0.6,-0.8,0.11,0.25],0.09)
 cost_of_equity_beta_is_1 = finance.CAPM_BETA1(0.02,0.09)
 
 
 #Convert daily price change from list of string to list of float 
-note_files = open('price_change.txt','r')
+note_files = open('%price_change.txt','r')
 x = note_files.read()
 b = x.split('\n')
 a = b
@@ -25,7 +24,7 @@ for element in a:
     daily_price_change.append(float(element))
     
 
-########################################################################################
+################################################################################################################################################################################
 
 mystat.find_robust_central_tendency(daily_price_change)
 
@@ -40,12 +39,12 @@ standard_deviation_samp = np.sqrt(population_variance)
 print('standard_DEVIATION:',standard_deviation_samp)  
 
 
-########################################################################################
+################################################################################################################################################################################
 #Gaussian distribution also known as the normal distribution 
 
 
 #https://www.desmos.com/calculator/pyviauerg0
-probablity = mystat.probablity_density_function(standard_deviation_samp,median,-0.06299598659374768,0.06299598659374768)
+probablity = mystat.probablity_density_function(standard_deviation_samp,median,-standard_deviation_samp,standard_deviation_samp)
 print(probablity)
 domain = np.arange(-4, 5, 1)
 plt.plot(domain, norm.pdf(domain,0,standard_deviation_samp))

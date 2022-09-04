13deea5e5c799a48b5035def0944b3a2e7d97c6c
Liang <lianghui732@gmail.com>
2022-09-04 08:48:00 -0400

    Update rblx_valuation.py

--- rblx_valuation.py
+++ rblx_valuation.py
@@ -6,40 +6,29 @@ import statistics
 import matplotlib.pyplot as plt
 from scipy.stats import norm 
 
-<<<<<<< HEAD
 #https://iopscience.iop.org/article/10.1088/1742-6596/1377/1/012016/pdf
 # Calculating WACC 
 ################################################################################################################################################################################
 #https://www.spglobal.com/marketintelligence/en/news-insights/latest-news-headlines/roblox-places-1b-of-senior-notes-at-par-to-yield-3-875-terms-67270896
 # https://www.researchgate.net/figure/Estimated-default-spreads-by-credit-rating_tbl2_288227112
-# Risk Free Rate we use is the 10-year bond 
-=======
 
----> read this <---
 
-  
 # Risk Free Rate we use is the 10-year treasury bond 
->>>>>>> 7c7ddd995f8faa189f82c940e4722a841a4e32b5
 #1) YTM Method(Can't apply)
 #2) taking the interest expense/Book Value of Debt 
 #3) --> Debt Rating approach <-- Using this
 #4) Synethetic Rating Approach 
 #5) Interest on Debt Approach 
-<<<<<<< HEAD
 
-<<<<<<< HEAD
 
 
 
 
 
 #list is the stock_market
-=======
->>>>>>> 7c7ddd995f8faa189f82c940e4722a841a4e32b5
-=======
+
 #https://www.spglobal.com/marketintelligence/en/news-insights/latest-news-headlines/roblox-places-1b-of-senior-notes-at-par-to-yield-3-875-terms-67270896
 
->>>>>>> 7c7ddd995f8faa189f82c940e4722a841a4e32b5
 ################################################################################################################################################################################
 # Converting string of list into float of list using note files 
 
@@ -47,7 +36,6 @@ note_files = open('%price_change.txt','r')
 x = note_files.read()
 b = x.split('\n')
 a = b
-
 
 daily_price_change = []
 for element in a:
@@ -61,14 +49,6 @@ Ratings = 'BB'
 US_YEAR_Treasury = 3.253
 rblx_cost_of_debt = finance.Debt_Rating_approach('BB',3.253)
 rblx_cost_of_equity  = finance.CAPM(3.253,0.11, daily_price_change,11.89)
-
-
-
-
-
-
-
-
 
 
 ################################################################################################################################################################################
@@ -96,23 +76,15 @@ plt.show()
 print('cost_of_debt:',rblx_cost_of_debt) #pass
 
 #2) ----> check cost_of_equity <----  
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 
 print('cost_of_equity',rblx_cost_of_equity) #pass
 
-
 #4)----> check WACC calculation < ---- 
 print('WACC:',finance.WACC(rblx_total_equity,rblx_total_debt,rblx_cost_of_equity,rblx_cost_of_debt,0.02))
-=======
-print('cost_of_equity',rblx_cost_of_equity) # pass
 
-=======
-print('cost_of_equity',rblx_cost_of_equity) # pass
 
->>>>>>> 7c7ddd995f8faa189f82c940e4722a841a4e32b5
-#3)----> check WACC calculation < ---- 
-print('WACC:',finance.WACC(rblx_total_equity,rblx_total_debt,rblx_cost_of_equity,rblx_cost_of_debt,0)) # pass 
 
->>>>>>> 7c7ddd995f8faa189f82c940e4722a841a4e32b5
+
+
+
+

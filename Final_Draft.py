86aa12f893cba094c828dfaa4a06d203bb0af361
Liang <lianghui732@gmail.com>
2022-09-05 19:33:31 -0400

    09/05/2022
    I do have intuition of understanding this model, but not fully.

--- Final_Draft.py
+++ Final_Draft.py
@@ -12,18 +12,21 @@ ticker = 'RBLX'
 df = pdr.DataReader(ticker, data_source='yahoo', start='2021-3-1')['Close']
 
 daily_percent_change = df.pct_change() 
-
-
-# Geometric Brownian Motion Combining With Monte_Carlo_Simulation  
-daily_percent_change = df.pct_change() 
-
 daily_percent_change.hist()
 
+
+
+
+
+# terms we need for monte_carlo simulation of brownian motion 
 u = daily_percent_change.mean()
 standard_deviation = daily_percent_change.std()
+variance = np.power(standard_deviation,2)
+drift = u - (1/2 * variance)
 
+z = norm.ppf(np.random.rand(50,10000))
+#print(z)
 
-
-
-
+daily_returns = np.exp(drift + standard_deviation * z)
+print(daily_returns)
 

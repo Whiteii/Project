c1bf28739bed48616701f2bdc13d5ef79fcbecbf
Liang <lianghui732@gmail.com>
2022-09-24 21:40:32 -0400

    9/24/2022

--- taylor_series_expansion.py
+++ taylor_series_expansion.py
@@ -1,10 +1,12 @@
 
+import fractions
 import sympy as smp 
 import numpy as np 
 import scipy as sp 
 import matplotlib.pyplot as plt 
 from scipy.misc import derivative  
-import math
+import math 
+from fractions import Fraction
 
 
 
@@ -12,35 +14,44 @@ import math
 x = smp.symbols('x', real = True) 
 
 
+
+
+sums = 0    
+
+
 # Enter the function that you want to approximate. 
 # ---> Enter Your Function Like This Then Run The Code <--- 
 
 
-
-
-#n # taking the derivative to N
-#c # Point To Approximate 
-
-sums = 0  
-def taylor_series_expansion(x = smp.symbols('x', real = True),f =  smp.cos(x),n = int(input("# Of Derivative")), point = int(input("Point To Approximate"))):
-    taylor_series = 0 
+def taylor_series_expansion(f = smp.cos(x)):  
+    x = smp.symbols('x', real = True) 
+    point = int(input("Point To Approximate")) 
+    n = int(input("# Of Derivative")) 
+    
+    taylor_series = 0  
+    
     for i in range(0,n + 1 ):  
-        x = smp.symbols('x', real = True) 
+         
+        
         dfn_dxn = smp.diff(f,x,i) 
         sub = dfn_dxn.subs([(x,point)]) 
+        
         denom = 1/math.factorial(i)
-        coeffient = (sub * denom)
-        terms = (coeffient) * (x-point) ** i 
-        taylor_series = terms + taylor_series 
-        #print(taylor_series)
+        coeffient = (sub * denom)                       
+        terms = coeffient * (x - point) ** i 
+        taylor_series = terms + taylor_series   
     return taylor_series
         
 a = taylor_series_expansion()
-print(a)
+print(a)  
+
+
+
+
+
     
         
-        
-    
+
     
     
     

dde5325c510f946c2fe1b255baeaec18e8bdb670
Liang <lianghui732@gmail.com>
2022-09-25 10:13:50 -0400

    Update taylor_series_expansion.py

--- Math_Related_Project/taylor_series_expansion.py
+++ Math_Related_Project/taylor_series_expansion.py
@@ -1,4 +1,4 @@
-import sympy as smp 
+import sympy as smp #symbolic mathematics
 import numpy as np 
 import scipy as sp 
 from sympy import diff, symbols, factorial, exp, pretty_print
@@ -7,48 +7,46 @@ from scipy.misc import derivative
 import math 
 
 
-
-
 x, c1 , c2 ,c3, c4 = smp.symbols('x c1 c2 c3 c4', real = True) 
 # More visual understanding of taylor series click here ---> https://www.desmos.com/calculator/3xohtl4swp
 
 # Enter the function that you want to approximate. 
 # ---> Enter Your Function Like This Then Run The Code <--- 
-"""def taylor_series_expansion(f_x):  
+def taylor_series_expansion(func):  
     x = smp.symbols('x', real = True) 
     point = int(input("Point To Approximate")) 
     n = int(input("# Of Derivative")) 
-    taylor_series = 0  
-    for i in range(0,n + 1 ):  
-        dfn_dxn = smp.diff(f_x,x,i) 
-        sub = dfn_dxn.subs([(x,point)]) 
-        denom = 1/math.factorial(i)
-        coeffient = (sub * denom)                       
-        terms = coeffient * (x - point) ** i 
-        taylor_series = terms + taylor_series   
-    return taylor_series
-        
+    results = 0  
+    for i in range(0,n):  
+        taylor_series = diff(func,x,i).subs(x,point) * (x - point) ** i / factorial(i) 
+        results = taylor_series + results               
+    return results  
+
+
 expand_cos_x = taylor_series_expansion(smp.cos(x))
-print(expand_cos_x) """  
+pretty_print(expand_cos_x) 
+
+
+
+expand_e_to_the_x = taylor_series_expansion(exp(1)**x)
+pretty_print(expand_e_to_the_x)
+ 
 
 
 
 
-x0 = 0
+'''x0 = 0
 n = 10
-
 func = smp.cos(x)
 results = func.subs([(c1,5)]) 
 apply_diff_to_function = diff(results,x,1) 
 #pretty_print(apply_diff_to_function) 
 dummy = 0
-
-
 for i in range(0,n): 
     something = diff(results,x,i).subs(x,0) * (x - x0) ** i / factorial(i)
     dummy = something + dummy
-    print(dummy)
-    pretty_print(dummy)
+    #print(dummy)
+    #pretty_print(dummy)'''
     
     
 

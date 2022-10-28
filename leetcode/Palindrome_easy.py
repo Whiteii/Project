c8f54f1434f038ae57c21fb987f37da02e43bd3b
Liang <lianghui732@gmail.com>
2022-10-28 19:21:15 -0400

    Start SQL & LeetCode

--- leetcode/Palindrome_easy.py
+++ leetcode/Palindrome_easy.py
@@ -1,46 +1,27 @@
 
 
-
-
-
-#>---- when something is a Palindrome ----< 
-# x = 1221 
-print(1221%10) #---> this is 1 
-print((1221/10)%10) # ---> this is 2   
-#It's like saying swapping 21 to 12 and if first two sequence = last two sequence => Palindrome 
-
-
-
-
-#>---- when something is not a Palindrome ----< 
-
-# x = -1221 
-#print(-1221%10) 
-
-
-
-
-
-
-
 def isPalindrome(x):
         rev = 0
         init_n = x
         if( x < 0):
             return False
         while x != 0:
-                rev = (rev*10) +  x % 10
-                x = x // 10 
-        
+               
+                rev = (rev*10) +  x % 10 
+                print(rev)
+                x = int(x / 10)
+                #print(x)
+                
+                
         if(rev == init_n):
-                    
+            #print(rev)
             return True 
         return False
             
-test = isPalindrome(1331)
-print(test)
+test = isPalindrome(1221)
+#print(test)
         
 
 
 
-
+print(2%10)

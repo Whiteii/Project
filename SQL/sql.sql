2edc283186d22506a04186195754c9f4f6b2ea90
Liang <lianghui732@gmail.com>
2022-11-12 22:20:22 -0500

    SQL GRIND

---
+++ SQL/sql.sql
@@ -0,0 +1,18 @@
+
+
+--CREATE TABLE baruch_student 
+---  (
+---  student_name VARCHAR(40) Primary Key,   
+---  major VARCHAR(40) NOT NULL
+
+ -- ); 
+
+
+--INSERT INTO baruch_student VALUES('Liang','Applied Math')  
+--INSERT INTO baruch_student VALUES('Elhamm','Applied Math')
+
+
+
+
+SELECT * FROM baruch_student
+

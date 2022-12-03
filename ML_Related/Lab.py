7a46530656be3df01d2108db5fff26bcaa7b3011
Liang <lianghui732@gmail.com>
2022-12-03 11:28:09 -0500

    ML

--- ML_Related/Lab.py
+++ ML_Related/Lab.py
@@ -4,7 +4,7 @@ import time
 
 
 # NumPy routines which allocate memory and fill arrays with value
-#a = np.zeros(4); print(f"np.zeros(4) :   a = {a}, a shape = {a.shape}, a data type = {a.dtype}") 
+a = np.zeros(4); print(f"np.zeros(4) :   a = {a}, a shape = {a.shape}, a data type = {a.dtype}") 
 #a = np.zeros((4,)); print(f"np.zeros(4,) :  a = {a}, a shape = {a.shape}, a data type = {a.dtype}") 
 #a = np.random.random_sample(4); print(f"np.random.random_sample(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}") 
 # These are single value for a 1-D result or a tuple(n,m,...) 
@@ -12,11 +12,22 @@ import time
 # NumPy routines which allocate memory and fill arrays with value but do not accept shape as input argument
 
 
-a = np.arange(4.);print(f"np.arange(4.):     a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+#a = np.arange(4.);print(f"np.arange(4.):     a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
 #a = np.random.rand(4);          print(f"np.random.rand(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}") 
 
 
 # Let construct a vector that is a = {2,3,4,1} 
-a = np.array([2,3,4,1]); print("np.array([2,3,4,1]: a = {a}, a data type = {a.dtype}") 
+#a = np.array([2,3,4,1]); print("np.array([2,3,4,1]: a = {a}, a data type = {a.dtype}") 
 #Let construct a vector that is a = {1,0,0,1}
-a = np.array([1,0,0,1]); print("np.array([1,0,0,1]): a = {a}, a data type = {a.dtype}")
+#a = np.array([1,0,0,1]); print("np.array([1,0,0,1]): a = {a}, a data type = {a.dtype}")
+
+
+
+a = np.arange(10)
+#print(f"a = {a}") 
+
+c = a[:3]
+#print(c)  
+
+c = a[3:]
+#print(c)
--- ML_Related/multiple_feature_linear_regression.py
+++ ML_Related/multiple_feature_linear_regression.py
@@ -1,8 +1,65 @@
 import numpy as np 
 
 x_train = np.array([[2104,5,1,45],[1416,3,2,40],[852,2,1,15]])
-print(f"x type : {type(x_train.shape)}, x_train.shape: {x_train.shape}")
-print(x_train)
-y_train = np.array([460,232,178])
-print(f"y type: {type(y_train)}, y_train.shape: {y_train.shape}")
-print(y_train)
+y_train = np.array([460,232,178]) 
+
+
+def predict_single_loop(x, w, b): 
+    """
+    single predict using linear regression
+    Args:
+      x (ndarray): Shape (n,) example with multiple features
+      w (ndarray): Shape (n,) model parameters    
+      b (scalar):  model parameter     
+    Returns:
+      p (scalar):  prediction
+    """
+    n = x.shape[0]
+    p = 0
+    for i in range(n):
+        p_i = x[i] * w[i]  
+        p = p + p_i         
+    p = p + b                
+    return p  
+
+# a faster approach is using vectoralization 
+
+def predict(x,w,b): 
+    p = np.dot(x,w) + b 
+    return p 
+
+
+
+x_vec = x_train[0,:]
+#print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+b_init = 785.1811367994083
+w_init = np.array([ 0.39, 18, -53, -26])
+f_wb = predict_single_loop(x_vec, w_init, b_init)
+#print(f_wb)
+
+
+def compute_cost(X, y, w, b): 
+    """
+    compute cost
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      cost (scalar): cost
+    """
+    m = X.shape[0] 
+    print('m',m)
+    cost = 0
+    for i in range(m):                                
+        f_wb_i = np.dot(X[i], w) + b     #(n,)(n,) = scalar (see np.dot) 
+        print(f_wb_i)
+        cost = cost + (f_wb_i - y[i])**2       #scalar
+    cost = cost / (2 * m)                      #scalar    
+    return cost  
+
+
+cost = compute_cost(x_train, y_train, w_init, b_init)
+print(f'Cost at optimal w : {cost}')

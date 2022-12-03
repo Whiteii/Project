e3594cb6cc3b29c6b4314f795ea8249bb1b7c623
Liang <lianghui732@gmail.com>
2022-12-03 09:18:05 -0500

    Update

--- ML_Related/Lab.py
+++ ML_Related/Lab.py
@@ -12,11 +12,11 @@ import time
 # NumPy routines which allocate memory and fill arrays with value but do not accept shape as input argument
 
 
-a = np.arange(4.);              print(f"np.arange(4.):     a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.arange(4.);print(f"np.arange(4.):     a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
 #a = np.random.rand(4);          print(f"np.random.rand(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}") 
 
 
 # Let construct a vector that is a = {2,3,4,1} 
-a = np.array([2,3,4,1]): print("np.array([2,3,4,1]: a = {a}, a data type = {a.dtype}") 
+a = np.array([2,3,4,1]); print("np.array([2,3,4,1]: a = {a}, a data type = {a.dtype}") 
 #Let construct a vector that is a = {1,0,0,1}
-a = np.array([1,0,0,1]): print("np.array([1,0,0,1]): a = {a}, a data type = {a.dtype}")
+a = np.array([1,0,0,1]); print("np.array([1,0,0,1]): a = {a}, a data type = {a.dtype}")
---
+++ ML_Related/multiple_feature_linear_regression.py
@@ -0,0 +1,8 @@
+import numpy as np 
+
+x_train = np.array([[2104,5,1,45],[1416,3,2,40],[852,2,1,15]])
+print(f"x type : {type(x_train.shape)}, x_train.shape: {x_train.shape}")
+print(x_train)
+y_train = np.array([460,232,178])
+print(f"y type: {type(y_train)}, y_train.shape: {y_train.shape}")
+print(y_train)

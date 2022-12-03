77d06624d8ee5c6baf91d0343f406d731cf92c09
Liang <lianghui732@gmail.com>
2022-12-03 15:53:14 -0500

    Update multiple_feature_linear_regression.py

--- ML_Related/multiple_feature_linear_regression.py
+++ ML_Related/multiple_feature_linear_regression.py
@@ -38,8 +38,7 @@ w_init = np.array([ 0.39, 18, -53, -26])
 f_wb = predict_single_loop(x_vec, w_init, b_init)
 #print(f_wb)
 
-
-def compute_cost(X, y, w, b): 
+def compute_cost(x, y, w, b): 
     """
     compute cost
     Args:
@@ -51,16 +50,55 @@ def compute_cost(X, y, w, b):
     Returns:
       cost (scalar): cost
     """
-    m = X.shape[0] 
+    m = x.shape[0] 
     print('m',m)
     cost = 0
     for i in range(m):                                
-        f_wb_i = np.dot(X[i], w) + b     #(n,)(n,) = scalar (see np.dot) 
-        print(f_wb_i)
-        cost = cost + (f_wb_i - y[i])**2       #scalar
-    cost = cost / (2 * m)                      #scalar    
+        f_wb_i = np.dot(x[i], w) + b     
+        cost = cost + (f_wb_i - y[i])**2       
+    cost = cost / (2*m)                         
     return cost  
 
-
 cost = compute_cost(x_train, y_train, w_init, b_init)
-print(f'Cost at optimal w : {cost}')
+print(f'Cost at optimal w : {cost}') 
+
+
+def compute_gradient(X, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar):       The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = X.shape #(number of examples, number of features)
+    print(X.shape)
+    
+    
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+
+    for i in range(m):                             
+        err = (np.dot(X[i], w) + b) - y[i]   
+        for j in range(n):                         
+            dj_dw[j] = dj_dw[j] + err * X[i, j]    
+        dj_db = dj_db + err                        
+    dj_dw = dj_dw / m                                
+    dj_db = dj_db / m                                
+        
+    return dj_db, dj_dw
+    
+
+#Compute and display gradient 
+tmp_dj_db, tmp_dj_dw = compute_gradient(x_train, y_train, w_init, b_init)
+print(f'dj_db at initial w,b: {tmp_dj_db}')
+print(f'dj_dw at initial w,b: \n {tmp_dj_dw}')
+
+
+
+ 

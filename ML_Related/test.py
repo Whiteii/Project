eb28960c1064e8953464595790fcbe430d7891c6
Liang <lianghui732@gmail.com>
2022-12-23 19:37:38 -0500

    Understanding

--- ML_Related/test.py
+++ ML_Related/test.py
@@ -1,31 +1,42 @@
 import numpy as np 
+import math 
 
 
+X_training_set = np.array([[2104, 5, 1, 45],[1416,3,2,40],[852,2,1,35]])
+y_train_set = np.array([460, 232, 178])
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+b_init = 785.1811367994083
 
-
-
-def cost_function(x,y,w,b):  
-    cost = 0 
+def compute_cost(x,y,w,b):
     m = x.shape[0]
-    #print(m)
-    #print(x.shape)
     for i in range(m): 
         f_w_b = np.dot(x[i],w) + b 
-        #print(f_w_b) 
-        cost = cost +(f_w_b - y[i]) ** 2 
-    cost = (1/(2*m)) * cost 
-    return cost  
-
-x_train = np.array([[2104, 5, 1, 45],[1416,3,2,40],[852,2,1,35]]) 
-y_train = np.array([460,5,2]) 
-w_init = np.array([ 0.39, 19, -53, -26])
-b_inital =  785   
-
-test = cost_function(x_train,y_train,w_init,b_inital)
+        cost = (f_w_b - [y]) ** 2  + cost 
+    cost = (1/ (2 * m)) + cost
+    return cost 
 
 
 
-print(x_train[0,1])
+def compute_gradient(x,y,w,b): 
+    m,n = X_training_set.shape
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+    for i in range(m): 
+        err = (np.dot(x[i],w) + b) - y[i]
+        for j in range(n):  
+            dj_dw[j] = dj_dw[j] + err * x[i,j] 
+        dj_db = dj_db + err 
+    dj_dw = dj_dw/m
+    dj_db = dj_db/m
+    
+    return dj_dw, dj_db
+    
+    
+    
+test = compute_gradient(X_training_set,y_train_set,w_init,b_init)   
+print(test)
+
+
 
 
 
